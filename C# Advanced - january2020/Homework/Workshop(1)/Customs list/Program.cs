3b1317aac1e3722b0ea3b0c76dfeb432b26ac604
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-04 18:01:44 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Workshop(1)/Customs list/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+
+namespace CustomDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomList<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+
+            Debug.Assert(list[2] == 2);
+            Debug.Assert(list.Count == 10);
+            list.RemoveAt(2);
+            Debug.Assert(list[2] == 3);
+            Debug.Assert(list[3] == 4);
+            Debug.Assert(list.Count == 9);
+            list.Reverse();
+            Debug.Assert(list.Count == 9);
+            Debug.Assert(list[0] == 9);
+            Debug.Assert(list[1] == 8);
+            Debug.Assert(list[2] == 7);
+        }
+    }
+}
