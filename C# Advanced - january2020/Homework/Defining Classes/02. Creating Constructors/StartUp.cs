8836bc0a61084422123f1eedb35315ce05cea6e8
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-04 17:47:19 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Defining Classes/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
