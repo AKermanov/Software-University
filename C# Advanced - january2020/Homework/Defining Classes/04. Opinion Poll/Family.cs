49c0cd9279352f9252b0043222e1ab3877d16e06
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-04 17:49:43 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Defining Classes/04. Opinion Poll/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+   public class Family
+    {
+		private List<Person> familyMembers;
+
+		public Family()
+		{
+			this.familyMembers = new List<Person>();
+		}
+
+		public List<Person> FamilyMembers
+		{
+			get { return familyMembers; }
+			set { familyMembers = value; }
+		}
+		public void AddMember(Person person)
+		{
+			this.familyMembers.Add(person);
+		}
+		public Person GetOldestMember()
+		{
+		    Person person = familyMembers.OrderByDescending(a => a.Age).FirstOrDefault();
+		   return person;
+		}
+	}
+}
---
+++ C# Advanced - january2020/Homework/Defining Classes/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+            var persons = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                var member = Console.ReadLine().Split();
+                var name = member[0];
+                var age = int.Parse(member[1]);
+                var currParson = new Person(name, age);
+                persons.Add(currParson);
+            }
+            var over30 = persons.OrderBy(x => x.Name).Where(x => x.Age > 30).ToList();
+            foreach (var item in over30)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+           
+        }
+    }
+}
