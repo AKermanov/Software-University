61ac0f0eb1e01e0a2e84b63378df61f22902276a
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-04 17:48:39 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Defining Classes/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family family = new Family();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);    
+            }
+
+            Person oldesMembet = family.GetOldestMember();
+
+            Console.WriteLine($"{oldesMembet.Name} {oldesMembet.Age}");
+        }
+    }
+}
