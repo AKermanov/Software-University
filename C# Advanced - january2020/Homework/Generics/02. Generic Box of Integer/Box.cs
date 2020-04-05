f32e4fe67029c216e5c2d0c177fd1ae32cf46ab4
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 21:56:52 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseGenerics
+{
+   public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"System.Int32: {this.Value}";
+        }
+    }
+}
