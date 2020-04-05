46cd0d8ebdb7ac86a66d34fe70bbcf58c742efc8
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 21:58:33 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/03. Generic Swap Method Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>(string.Empty);
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                box.Values.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = indexes[0];
+            int b = indexes[1];
+
+            box.Swap(a, b);
+
+            Console.WriteLine(box.ToString());
+
+        }
+    }
+}
