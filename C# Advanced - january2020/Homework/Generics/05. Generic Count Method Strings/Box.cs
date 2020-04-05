b7876ebb802944d56a7d9a94d87ff58659f3c1a1
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:01:26 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/05. Generic Count Method Strings/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Values = new List<T>();
+        }
+        public List<T> Values { get; set; }
+
+        public int GreaterValuesThan(T targetItem)
+            //where T : ICloneable
+        {
+            int counter = 0;
+            foreach (var value in this.Values)
+            {
+                if (value.CompareTo(targetItem) > 0)
+                {
+                    counter++;
+                }
+               
+
+            }
+
+            return counter;
+        }
+
+        public void Swap(int a, int b)
+        {
+            bool isInRage = a >= 0 && a < this.Values.Count && b >= 0 && b < this.Values.Count;
+            if (!isInRage)
+            {
+                throw new InvalidOperationException("Values are not in range!");
+            }
+
+            T tempValue = this.Values[a];
+            this.Values[a] = this.Values[b];
+            this.Values[b] = tempValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.Values)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            string result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ C# Advanced - january2020/Homework/Generics/05. Generic Count Method Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<string> box = new Box<string>(string.Empty);
+
+            for (int i = 0; i < count; i++)
+            {
+              string input = Console.ReadLine();
+                box.Values.Add(input);
+            }
+
+            string targerItem = Console.ReadLine();
+           int result = box.GreaterValuesThan(targerItem);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
