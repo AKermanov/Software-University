5133b0e321ff31090a0359cff1ece5d3b78b263a
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:13:57 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Iterators and Comparators/06. *Equality Logic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            SortedSet<Person> sortedPeople = new SortedSet<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                Person person = new Person(name, age);
+
+                people.Add(person);
+                sortedPeople.Add(person);
+            }
+
+            Console.WriteLine(people.Count);
+            Console.WriteLine(sortedPeople.Count);
+        }
+    }
+}
