224c02d9f2ad9a7fd0bfb7ac9d9d48a6ab5414d0
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 21:59:58 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/04. Generic Swap Method Integers/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp90
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Values = new List<T>();
+        }
+        public List<T> Values { get; set; }
+
+        public void Swap(int a, int b)
+        {
+            bool isInRage = a >= 0 && a < this.Values.Count && b >= 0 && b < this.Values.Count;
+            if (!isInRage)
+            {
+                throw new InvalidOperationException("Values are not in range!");
+            }
+
+            T tempValue = this.Values[a];
+            this.Values[a] = this.Values[b];
+            this.Values[b] = tempValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in this.Values)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            string result = sb.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ C# Advanced - january2020/Homework/Generics/04. Generic Swap Method Integers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>(0);
+
+            for (int i = 0; i < count; i++)
+            {
+               int input = int.Parse(Console.ReadLine());
+                box.Values.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = indexes[0];
+            int b = indexes[1];
+
+            box.Swap(a, b);
+
+            Console.WriteLine(box.ToString());
+
+        }
+    }
+}
