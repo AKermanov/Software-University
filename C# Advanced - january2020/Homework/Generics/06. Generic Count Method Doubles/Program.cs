087529e4da6d431f7320a6358e34d99cbc3e066c
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:02:36 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/06. Generic Count Method Doubles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Box<double> box = new Box<double>(0);
+
+            for (int i = 0; i < count; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                box.Values.Add(input);
+            }
+
+            double targerItem = double.Parse(Console.ReadLine());
+            int result = box.GreaterValuesThan(targerItem);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
