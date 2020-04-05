e04b492949a0d39e1ce0a661de8cc89bfca16fe6
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:03:26 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Generics/07. Tuple/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp90
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputPersonInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] inputPersonBeer = Console.ReadLine().Split();
+            string[] inputNumbersInfo = Console.ReadLine().Split();
+
+            string fullName = inputPersonInfo[0] + " " + inputPersonInfo[1];
+            string address = inputPersonInfo[2];
+
+            string name = inputPersonBeer[0];
+            int liters = int.Parse(inputPersonBeer[1]);
+
+            int myInte = int.Parse(inputNumbersInfo[0]);
+            double myDouble = double.Parse(inputNumbersInfo[1]);
+
+            MyTuple<string, string> personInfo = new MyTuple<string, string>(fullName, address);
+            MyTuple<string, int> personBEER = new MyTuple<string, int>(name, liters);
+            MyTuple<int, double> numberInfo = new MyTuple<int, double>(myInte, myDouble);
+
+            Console.WriteLine(personInfo);
+            Console.WriteLine(personBEER);
+            Console.WriteLine(numberInfo);
+
+        }
+    }
+}
