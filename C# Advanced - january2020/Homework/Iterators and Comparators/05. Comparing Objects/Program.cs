1821e58f800b4a58457b72cbb37800e1774812ee
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:12:52 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Iterators and Comparators/05. Comparing Objects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] personInfo = command.Split();
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+                string town = personInfo[2];
+
+                Person person = new Person(name, age, town);
+
+                people.Add(person);
+
+                command = Console.ReadLine();
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            Person targetPerson = people[n - 1];
+            int matches = 1;
+            foreach (var person in people)
+            {
+                if (person.CompareTo(targetPerson) == 0 && !person.Equals(targetPerson))
+                {
+                    matches++;
+                }
+            }
+
+            if (matches == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matches} {people.Count-matches} {people.Count}");
+            }
+        }
+    }
+}
