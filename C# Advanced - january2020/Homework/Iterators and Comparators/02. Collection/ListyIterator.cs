444af4474b6b12468afed9f13556073047b66f4b
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-05 22:10:30 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Homework/Iterators and Comparators/02. Collection/ListyIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+        private int index;
+        public ListyIterator(List<T> elemenets)
+        {
+            this.elements = elemenets;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            bool isNextElement = this.HasNext();
+            if (isNextElement)
+            {
+                this.index++;
+                return true;
+            }
+            return HasNext();
+        }
+        public bool HasNext()
+        {
+            if (this.index < elements.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            return $"{elements[index]}";
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in elements)
+            {
+                yield return item; 
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced - january2020/Homework/Iterators and Comparators/02. Collection/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var command = Console.ReadLine();
+            var cmdArgs = command
+                .Split()
+                .Skip(1)
+                .ToList();
+            ListyIterator<string> listyIterator = new ListyIterator<string>(cmdArgs);
+
+            while (command != "END")
+            {
+                if (command == "Print")
+                {
+                    try
+                    {
+                        Console.WriteLine(listyIterator.Print()); 
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(listyIterator.Move()); 
+                }
+                else if (command == "HasNext" )
+                {
+                    Console.WriteLine(listyIterator.HasNext());
+                }
+                else if (command == "PrintAll")
+                {
+                    Console.WriteLine(string.Join(" ", listyIterator));
+                }
+                command = Console.ReadLine();
+            }
+            
+        }
+    }
+}
