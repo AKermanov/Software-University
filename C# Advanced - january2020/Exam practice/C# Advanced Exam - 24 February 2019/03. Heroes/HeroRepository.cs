22a02bd8e5deafb1fb8197f76d1bafb9397807f5
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-06 09:59:24 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Exam practice/C# Advanced Exam - 24 February 2019/03. Heroes/HeroRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Heroes
+{
+   public class HeroRepository
+    {
+        private List<Hero> data;
+        public HeroRepository()
+        {
+            this.data = new List<Hero>();
+        }
+
+        public void Add (Hero hero)
+        {
+            this.data.Add(hero);
+        }
+        public void Remove(string name)
+        {
+            var heroToRemove = this.data.FirstOrDefault(x => x.Name == name);
+            this.data.Remove(heroToRemove);
+        }
+        public Hero GetHeroWithHighestStrength()
+        {
+            var sort = this.data.Select(x => x.Item.Strenght);
+            var heroToreturn = this.data[0];
+            return heroToreturn;
+        }
+        public Hero GetHeroWithHighestAbility()
+        {
+            var sort = this.data.Select(x => x.Item.Ability);
+            var heroToreturn = this.data[0];
+            return heroToreturn;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            var sort = this.data.Select(x => x.Item.Intelligence);
+            var heroToreturn = this.data[0];
+            return heroToreturn;
+        }
+
+        public int Count => this.data.Count();
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in data)
+            {
+                sb.AppendLine($"Hero: {item.Name} - {item.Level}");
+                sb.AppendLine($"{item.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
