c3b1363738782f78c9c6441dbd36eaec4f32c3e8
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-06 09:52:50 +0300

    Add files via upload

---
+++ C# Advanced - january2020/Exam practice/C# Advanced Retake Exam - 13 August 2019/03. Aquarium Adventure/Aquarium.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AquariumAdventure
+{
+    public class Aquarium
+    {
+        private HashSet<Fish> fishInPool;
+        private Aquarium()
+        {
+            this.fishInPool = new HashSet<Fish>();
+        }
+        public Aquarium(string name, int capacity, int size)
+            : this()
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Size = size;
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Size { get; set; }
+
+        public void Add(Fish fish)
+        {
+            if (!this.fishInPool.Any(f => f.Name == fish.Name) &&
+                !(this.fishInPool.Count >= this.Capacity))
+            {
+                this.fishInPool.Add(fish);
+            }
+        }
+        public bool Remove(string name)
+        {
+            int removeEl = 0;
+            if (fishInPool.Any())
+            {
+                removeEl = fishInPool.RemoveWhere(x => x.Name == name);
+            }
+            bool isRemoved = removeEl == 1 ? true : false;
+            return isRemoved;
+        }
+
+        public Fish FindFish(string name)
+        {
+
+            Fish fish = fishInPool.FirstOrDefault(x => x.Name == name);
+            return fish;
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Aquarium: {this.Name} ^ Size: {this.Size}");
+            foreach (var item in fishInPool)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced - january2020/Exam practice/C# Advanced Retake Exam - 13 August 2019/03. Aquarium Adventure/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace AquariumAdventure
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Sample Code Usage:
+
+            //Initialize Aquarium
+            Aquarium aquarium = new Aquarium("Ocean", 5, 3);
+
+            //Initialize Fish
+            Fish fish = new Fish("Goldy", "gold", 4);
+
+            //Print Fish
+            Console.WriteLine(fish.ToString());
+
+            //Fish: Goldy
+            //Color: gold
+            //Number of fins: 4
+
+            //Add Fish
+            aquarium.Add(fish);
+
+            //Remove Fish
+            Console.WriteLine(aquarium.Remove("Goldy")); // true
+
+            Fish secondFish = new Fish("Dory", "blue", 2);
+            Fish thirdFish = new Fish("Nemo", "orange", 5);
+            Fish foruthFish = new Fish("Nemo1", "orange", 5);
+            Fish fifthFish = new Fish("Nemo2", "orange", 5);
+
+            //Add fish
+            aquarium.Add(secondFish);
+            aquarium.Add(thirdFish);
+            aquarium.Add(foruthFish);
+            aquarium.Add(fifthFish);
+
+            //Print Aquarium report
+            Console.WriteLine(aquarium.Report());
+
+            //Aquarium Info:
+            //Aquarium: Ocean ^ Size: 15
+            //Fish: Dory
+            //Color: blue
+            //Number of fins: 2
+            //Fish: Nemo
+            //Color: orange
+            //Number of fins: 5
+
+
+        }
+    }
+}
