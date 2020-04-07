6f0e7556bda403e7e8ee8d9e78841adfd4d97bdf
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-07 19:25:23 +0300

    Add files via upload

---
+++ C# OOP - february 2020/Homework/Working with Abstraction/02. Cars Salesman/StartUp.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P02_CarsSalesman
+{
+    
+    
+    class CarSalesman
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            List<Engine> engines = new List<Engine>();
+            int engineCount = int.Parse(Console.ReadLine());
+            ParseCommandForCreatingEngine(engines, engineCount);
+            int carCount = int.Parse(Console.ReadLine());
+            ParseCommandFoCreatingCar(cars, engines, carCount);
+
+            PrintOnCOnsole(cars);
+        }
+
+        private static void ParseCommandFoCreatingCar(List<Car> cars, List<Engine> engines, int carCount)
+        {
+            for (int i = 0; i < carCount; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = parameters[0];
+                string engineModel = parameters[1];
+                Engine engine = engines.FirstOrDefault(x => x.Model == engineModel);
+                CreateCar(cars, parameters, model, engine);
+            }
+        }
+
+        private static void ParseCommandForCreatingEngine(List<Engine> engines, int engineCount)
+        {
+            for (int i = 0; i < engineCount; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string model = parameters[0];
+                int power = int.Parse(parameters[1]);
+                CreateEngine(engines, parameters, model, power);
+            }
+        }
+
+        private static void PrintOnCOnsole(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+        private static void CreateCar(List<Car> cars, string[] parameters, string model, Engine engine)
+        {
+            int weight = -1;
+
+            if (parameters.Length == 3 && int.TryParse(parameters[2], out weight))
+            {
+                cars.Add(new Car(model, engine, weight));
+            }
+            else if (parameters.Length == 3)
+            {
+                string color = parameters[2];
+                cars.Add(new Car(model, engine, color));
+            }
+            else if (parameters.Length == 4)
+            {
+                string color = parameters[3];
+                cars.Add(new Car(model, engine, int.Parse(parameters[2]), color));
+            }
+            else
+            {
+                cars.Add(new Car(model, engine));
+            }
+        }
+
+        private static void CreateEngine(List<Engine> engines, string[] parameters, string model, int power)
+        {
+            int displacement = -1;
+
+            if (parameters.Length == 3 && int.TryParse(parameters[2], out displacement))
+            {
+                engines.Add(new Engine(model, power, displacement));
+            }
+            else if (parameters.Length == 3)
+            {
+                string efficiency = parameters[2];
+                engines.Add(new Engine(model, power, efficiency));
+            }
+            else if (parameters.Length == 4)
+            {
+                string efficiency = parameters[3];
+                engines.Add(new Engine(model, power, int.Parse(parameters[2]), efficiency));
+            }
+            else
+            {
+                engines.Add(new Engine(model, power));
+            }
+        }
+    }
+
+}
