13d99b00be256db7d1b49d8c6a4f18ecf8c4cd80
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-08 19:10:22 +0300

    Add files via upload

---
+++ C# OOP - february 2020/Homework/Inheritance/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿namespace NeedForSpeed
+{
+   public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+        public virtual double FuelConsumption => 1.25; 
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+       public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= this.FuelConsumption;
+        }
+    }
+}
