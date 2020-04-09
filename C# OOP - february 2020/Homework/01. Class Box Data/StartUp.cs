9ebd65c9e4f1e10906e7d21bc29de6d1e3656716
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-04-09 16:20:59 +0300

    Add files via upload

---
+++ C# OOP - february 2020/Homework/01. Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				var lenght = double.Parse(Console.ReadLine());
+				var width = double.Parse(Console.ReadLine());
+				var height = double.Parse(Console.ReadLine());
+
+				var box = new Box(lenght , width, height);
+				Console.WriteLine($"Surface Area - {box.SurfaceArea()}");
+				Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea()}");
+				Console.WriteLine($"Volume - {box.Volume()}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+        }
+    }
+}
