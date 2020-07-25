661c6d966cc35039ebcd6b6675b3d119c0a118a8
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-07-25 09:45:45 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/ADO.NET/08. Increase Minion Age/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace _8._Increase_Minion_Age
+{
+    class Program
+    {
+        private const string ConnectionString = @"
+                    Server=DESKTOP-5FMQC2G\SQLEXPRESS; 
+                    Database=MinionsDB; 
+                    Integrated Security=true;";
+        private const string MinionsNamesToTilteAndAgePlusOneQueryPath = @"..\..\Minion names to title Age++ query.sql";
+
+        public static void Main()
+        {
+            IEnumerable<int> minionsIds = Console.ReadLine()
+                .Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                string cmdText = File.ReadAllText(MinionsNamesToTilteAndAgePlusOneQueryPath)
+                    .Replace("@idCollection", string.Join(", ", minionsIds));
+                using (SqlCommand command = new SqlCommand(cmdText, connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine($"Affected rows: {rowsAffected}");
+                }
+            }
+        }
+    }
+}
+
