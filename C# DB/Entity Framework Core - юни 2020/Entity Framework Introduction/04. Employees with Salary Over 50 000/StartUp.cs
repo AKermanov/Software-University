08d615619324d68361ac63102467be7ef2c6c8dd
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-07-27 09:22:15 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+
+namespace SoftUni
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using var context = new SoftUniContext();
+            var result = GetEmployeesWithSalaryOver50000(context);
+
+            Console.WriteLine(result);
+        }
+
+        //3.Employees Full Information
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var emplyees = context.Employees
+                .Where(e => e.Salary > 50000) //return IQuaryble<Employee>, и е закачено за BD
+               .Select(e => new               //създаваме анонимен обект в който селектираме само това което ни трябва
+               {                              //ползваме lazy loading
+                   e.FirstName,
+                   e.Salary
+               })
+                .OrderBy(e => e.FirstName)
+                .ToList();                    //когато му извика tolist, то вече не закачено за базата
+                                              //и не правим промени по базата
+            foreach (var e in emplyees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
