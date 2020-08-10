61721f43070666c509969d89f16af4a02b5451cd
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-08-10 18:55:27 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/Advanced Querying/01. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+
+            string result = GetBooksByAgeRestriction(db, input);
+
+            Console.WriteLine(result);
+        }
+
+        //1. Age Restriction
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            List<string> booksTitles = context
+                .Books
+                .AsEnumerable()
+                .Where(b => b.AgeRestriction.ToString().ToLower() == command.ToLower())
+                //когато кажем на на една енумерация toString, връща ни стринга зад числото в енумерацията
+                .Select(b => b.Title)
+                .OrderBy(bt => bt)
+                .ToList();
+
+            return String.Join(Environment.NewLine, booksTitles);
+        }
+        /*14. Increase Prices
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var books = context
+                            .Books
+                            .Where(b => b.ReleaseDate.Value.Year < 2010);
+            foreach (var b in books)
+            {
+                b.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+        */
+    }
+
+}
