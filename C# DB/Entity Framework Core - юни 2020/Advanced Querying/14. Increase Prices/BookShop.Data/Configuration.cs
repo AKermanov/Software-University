e76867d1621a3f065bb2cd788abe98e1d3b45038
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-08-20 08:56:01 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/Advanced Querying/14. Increase Prices/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString 
+            => @"Server=TW1T4Y\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+    }
+}
