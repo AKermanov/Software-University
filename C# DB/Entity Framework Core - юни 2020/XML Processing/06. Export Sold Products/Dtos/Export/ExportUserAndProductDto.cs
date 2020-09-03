52d115a8a6295aad559703baf299213b596602da
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-09-03 19:38:50 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/06. Export Sold Products/Dtos/Export/ExportUserAndProductDto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+   public class ExportUserAndProductDto
+    {
+
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlElement("users")]
+        public ExportUserDto[] Users { get; set; }
+    }
+
+    [XmlType("User")]
+    public class ExportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int? Age { get; set; }
+
+        [XmlElement("SoldProducts")]
+        public ExportSoldProductDto SoldProducts { get; set; }
+    }
+
+    
+    public class ExportSoldProductDto
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("products")]
+        public UserSoldProductDto[] Products { get; set; }
+    }
+
+    [XmlType("Product")]
+    public class UserSoldProductDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+    }
+}
