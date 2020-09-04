6dd4488d6bf9cafc11106255980948a8df429e38
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-09-04 06:58:25 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/09. Import Suppliers/DataTransferObject/ImportSupplierDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObject
+{
+    [XmlType("Supplier")]
+   public class ImportSupplierDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImported")]
+        public bool IsImported { get; set; }
+    }
+}
---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using CarDealer.Data;
+using CarDealer.DataTransferObject;
+using CarDealer.Models;
+using CarDealer.XMLHelper;
+using System.Linq;
+using System.Xml;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+
+
+        //09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var suppliersDtos = XmlConverter.Deserializer<ImportSupplierDto>(inputXml, "Suppliers");
+
+            var result = suppliersDtos.Select(s => new Supplier
+            {
+                Name = s.Name,
+                IsImporter = s.IsImported
+
+            })
+            .ToArray();
+
+            context.Suppliers.AddRange(result);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliersDtos.Length}";
+        }
+    }
+}
