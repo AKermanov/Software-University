30d8dfb9ea37aed389e98a5e70541a394419d944
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-09-12 08:57:28 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/14. Export Cars With Distance/DataTransferObject/ExportCarPartDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObject
+{
+    [XmlType("part")]
+   public class ExportCarPartDto
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("price")]
+        public decimal Price { get; set; }
+    }
+}
