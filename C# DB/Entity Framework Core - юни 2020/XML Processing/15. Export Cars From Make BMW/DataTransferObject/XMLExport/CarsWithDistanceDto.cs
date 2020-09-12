f64f06f2284f744a9933e5f589eb8086c050ee32
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-09-12 08:58:13 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/15. Export Cars From Make BMW/DataTransferObject/XMLExport/CarsWithDistanceDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DataTransferObject.XMLExport
+{
+    [XmlType("car")]
+   public class CarsWithDistanceDto
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("travelled-distance")]
+        public long Distance { get; set; }
+
+    }
+}
