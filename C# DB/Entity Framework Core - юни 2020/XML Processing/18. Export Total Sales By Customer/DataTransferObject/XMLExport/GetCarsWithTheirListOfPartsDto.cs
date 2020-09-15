3f5645e91a96e85f742a031ad8121505a527ab47
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2020-09-15 19:24:05 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/XML Processing/18. Export Total Sales By Customer/DataTransferObject/XMLExport/GetCarsWithTheirListOfPartsDto.cs
@@ -0,0 +1,33 @@
+﻿namespace CarDealer.Dtos.Export
+{
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    [XmlType("car")]
+    public class GetCarsWithTheirListOfPartsDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("travelled-distance")]
+        public long TravelledDistance { get; set; }
+
+        [XmlArray(ElementName = "parts")]
+        public List<ExportPartDto1> Parts { get; set; }
+    }
+
+   
+        [XmlType("part")]
+        public class ExportPartDto1
+        {
+            [XmlAttribute("name")]
+            public string Name { get; set; }
+
+            [XmlAttribute("price")]
+            public decimal Price { get; set; }
+        }
+    
+}
