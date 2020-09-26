f24bf5d3ffc0590b5494854a8ade222fc2f921fa
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2020-09-26 13:43:37 +0300

    Add files via upload

---
+++ C# DB/Entity Framework Core - юни 2020/JSON Processing/13. Import Sales/DTO/CarDto.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+   public class CarDto
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        [JsonProperty("partsId")]
+        public int[] PartsId { get; set; }
+    }
+}
