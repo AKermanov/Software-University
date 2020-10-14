8a1ae7fa7ba1508d19a909d4a7b703455df405d4
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2020-10-14 14:58:50 +0300

    Add files via upload

---
+++ Data Structures Fundamentals (with C#)/01. Linear data structures/07.BalancedParantheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,45 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if (string.IsNullOrEmpty(parentheses) || parentheses.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            var openBrakets = new Stack<char>(parentheses.Length / 2);
+
+            foreach (var curentBraket in parentheses)
+            {
+                char expectedCharecter = default;
+                switch (curentBraket)
+                {
+                    case ')':
+                        expectedCharecter = '(';
+                        break;
+                    case ']':
+                        expectedCharecter = '[';
+                        break;
+                    case '}':
+                        expectedCharecter = '{';
+                        break;
+                    default:
+                        openBrakets.Push(curentBraket);
+                        break;
+                }
+
+                if (expectedCharecter != default && openBrakets.Pop() != expectedCharecter)
+                {
+                    return false;
+                }
+            }
+
+            return openBrakets.Count == 0;
+        }
+    }
+}
