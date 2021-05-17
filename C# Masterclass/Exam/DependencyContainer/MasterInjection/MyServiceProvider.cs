f1fa48cbae5cdce1a6a2c11dab3e49edc464bd1a
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2021-05-17 09:49:40 +0300

    Add files via upload

---
+++ C# Masterclass/Exam/DependencyContainer/MasterInjection/MyServiceProvider.cs
@@ -0,0 +1,59 @@
+﻿namespace MasterInjection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MyServiceProvider : IMyServiceProvider
+    {
+        private readonly Dictionary<Type, Type> container;
+
+        public MyServiceProvider()
+        {
+            this.container = new Dictionary<Type, Type>();
+        }
+
+        public void Add<TSource, TDestination>()
+            where TDestination : TSource
+        {
+            this.container[typeof(TSource)] = typeof(TDestination);
+            this.container[typeof(TDestination)] = typeof(TDestination);
+        }
+
+        public object CreateInstance(Type type)
+        {
+            if (type is null)
+            {
+                return default;
+            }
+
+            if (!this.container.ContainsKey(type))
+            {
+                return null;
+            }
+
+            type = this.container[type];
+            var constructor = type.GetConstructors().OrderBy(x => x.GetParameters().Count()).FirstOrDefault();
+
+            if (constructor is null)
+            {
+                return default;
+            }
+
+            var parameters = constructor.GetParameters();
+            var paramVal = new List<object>();
+            foreach (var parameter in parameters)
+            {
+                var parameterValue = CreateInstance(parameter.ParameterType);
+                paramVal.Add(parameterValue);
+            }
+
+            var obj = constructor.Invoke(paramVal.ToArray());
+            return obj;
+
+        }
+
+        public T CreateInstance<T>()
+             => (T)CreateInstance(typeof(T));
+    }
+}
---
+++ C# Masterclass/Exam/IOProvider/DWAsync/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DWAsync
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            await ReadCharacters();
+        }
+
+        static async Task ReadCharacters()
+        {
+            var dirInfo = new DirectoryInfo(@"..\..\..\Files");
+            var files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);
+            //Console.WriteLine(f.Length.ToString());
+
+            for (int i = 1; i <= files.Length; i++)
+            {
+                using (StreamReader reader = new StreamReader(@$"..\..\..\Files\{i}.txt"))
+                {
+                    Console.WriteLine("Async Read File has started");
+                    String result = await reader.ReadToEndAsync();
+                    await WriteFileAsync(@$"..\..\..\Files\", $"{i}-coppy.txt", result);
+                    Console.WriteLine("Async Read File has completed");
+                }
+            }
+        }
+
+        static async Task WriteFileAsync(string dir, string file, string content)
+        {
+            Console.WriteLine("Async Write File has started");
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(dir, file)))
+            {
+                await outputFile.WriteAsync(content);
+            }
+            Console.WriteLine("Async Write File has completed");
+        }
+
+    }
+}
